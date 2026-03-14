b72bd80fbd083038500d57e64ad7aab82fbbc346
Kingsley Amoke <klordbravo@gmail.com>
2026-03-14 21:56:16 +0000

    initialized firebase app

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,18 +1,19 @@
 // config/firebase.config.ts
 import { configDotenv } from 'dotenv';
 import * as admin from 'firebase-admin';
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 configDotenv();
 
-// const config = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -22,7 +23,7 @@ admin.initializeApp({
   }),
 });
 
-// firebase.initializeApp(config);
+initializeApp(config);
 
 export const firestore = admin.firestore();
 export const auth = admin.auth();
